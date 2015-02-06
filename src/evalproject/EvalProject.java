1e52c81508439b13a2a2a4188fc9f7b0b2789b11
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2015-02-06 14:42:17 +0100

    initial commit

---
+++ src/evalproject/EvalProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evalproject;
+
+/**
+ *
+ * @author Leo
+ */
+public class EvalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Experiment experiment = new Experiment();
+    }
+}
---
+++ src/evalproject/Experiment.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evalproject;
+
+import java.io.File;
+
+/**
+ *
+ * @author Leo
+ */
+class Experiment {
+    // experiment.csv
+    protected File designFile = null;
+}
