617dfd0fc45e379fb2805f10347bc45b3a85ccb3
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2015-03-06 15:26:42 +0100

    my son will be called Horvath Leonardo

--- src/evalproject/AnimationCircle.java
+++ src/evalproject/AnimationCircle.java
@@ -1,8 +1,6 @@
 package evalproject;
 
 import fr.lri.swingstates.animations.Animation;
-import fr.lri.swingstates.canvas.CEllipse;
-import java.util.ArrayList;
 
 /**
  * Display animation for the only targeted 
@@ -11,44 +9,33 @@ import java.util.ArrayList;
  */
  class AnimationCircle extends Animation {
         double translatex, translatey;
+        double start_x, start_y;
         Experiment experiment;
-        ArrayList start_x;
-        ArrayList start_y;
  
         public AnimationCircle(Experiment exp) {
-            super();
-            translatex = 20;
-            translatey = 20;
-            experiment = exp;
-            start_x = new ArrayList();
-            start_y = new ArrayList();
+                super();
+                translatex = 20;
+                translatey = 20;
+                experiment = exp;
         }
  
         public void step(double t) {
                 if (t < 0.5) {
-                    experiment.getMovingNonTarget().translateBy(0, 4);
+                    experiment.getTarget().translateBy(0, 4);
                 }
                 else {
-                    experiment.getMovingNonTarget().translateBy(0, -4);
+                    experiment.getTarget().translateBy(0, -4);
                 }
         }
         
         // save starting coords
         public void doStart() {
-            experiment.getMovingNonTarget().reset();
-            while(experiment.getMovingNonTarget().hasNext()) {
-                start_x.add(experiment.getMovingNonTarget().nextShape().getCenterX());
-                start_y.add(experiment.getMovingNonTarget().nextShape().getCenterY());
-            }
+            start_x = experiment.getTarget().getCenterX();
+            start_y = experiment.getTarget().getCenterY();
         }
         
         // put coords back
         public void doStop() {
-            experiment.getMovingNonTarget().reset();
-            int i = 0;
-            while(experiment.getMovingNonTarget().hasNext()) {
-                experiment.getMovingNonTarget().nextShape().translateTo((double) start_x.get(i), (double) start_y.get(i));
-                i++;
-            }
+            experiment.getTarget().translateTo(start_x, start_y);
         }
  }
--- src/evalproject/EvalProject.java
+++ src/evalproject/EvalProject.java
@@ -13,7 +13,8 @@ public class EvalProject {
     public static void main(String[] args) {
         Experiment experiment = new Experiment("0", 1, 1, new File("experiment.csv"));
         
-        // uncomment next line to load a debug version of the csv starting from VV1VV2
+        // uncomment next lines to load debug version of the csv starting from VV
         //Experiment experiment = new Experiment("0", 1, 1, new File("experiment_debug.csv"));
+        //Experiment experiment = new Experiment("0", 1, 1, new File("experiment_debug_2.csv"));
     }
 }
